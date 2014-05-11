b9feb57a90be7e3cdbe64251bbbd6dffd77c2944
Rohit Girdhar <rohit@iiith>
2014-05-11 15:31:42 +0530

    combine word keys

---
+++ src/combineWordsKeys.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dir=$1
+list=`cat ${dir}ImageNames.txt`
+mkdir -p ${dir}wordkeys
+for i in $list
+do
+    paste ${dir}words/w_${i}.txt ${dir}keypoints/K_${i}.txt > ${dir}wordkeys/wk_$i.txt
+done
