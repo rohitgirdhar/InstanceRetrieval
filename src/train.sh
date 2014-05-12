99665b9978e12796badbc30dc2f90bda0291afcc
Rohit Girdhar <rohit@iiith>
2014-05-12 23:31:35 +0530

    train, check error successively

--- src/train.sh
+++ src/train.sh
@@ -12,16 +12,16 @@ OUTPUT_DIR=${2}/
 BRANCH_FACTOR=${3}
 NUM_CLUSTERS=${4}
 
-#make
-#./getDescriptors -i ${IMAGES_DIR} -o ${OUTPUT_DIR} --resize
+make
+./getDescriptors -i ${IMAGES_DIR} -o ${OUTPUT_DIR} --resize && \
 
-bash matlab_batcher.sh hikmeans \'${OUTPUT_DIR}\',${BRANCH_FACTOR},${NUM_CLUSTERS}
-bash matlab_batcher.sh parseTree \'${OUTPUT_DIR}\',${NUM_CLUSTERS}
-bash matlab_batcher.sh quantize \'${OUTPUT_DIR}\',${NUM_CLUSTERS}
-bash matlab_batcher.sh wordsInDir \'${OUTPUT_DIR}\',${NUM_CLUSTERS}
-bash combineWordsKeys.sh ${OUTPUT_DIR}
+bash matlab_batcher.sh hikmeans \'${OUTPUT_DIR}\',${BRANCH_FACTOR},${NUM_CLUSTERS} && \
+bash matlab_batcher.sh parseTree \'${OUTPUT_DIR}\',${NUM_CLUSTERS} && \
+bash matlab_batcher.sh quantize \'${OUTPUT_DIR}\',${NUM_CLUSTERS} && \
+bash matlab_batcher.sh wordsInDir \'${OUTPUT_DIR}\',${NUM_CLUSTERS} && \
+bash combineWordsKeys.sh ${OUTPUT_DIR} && \
 ./buildSearchIndex -o ${OUTPUT_DIR}
-mv ./ImageSearch $OUTPUT_DIR
 
+mv ./ImageSearch $OUTPUT_DIR
 rm -r ${OUTPUT_DIR}/MatFiles ${OUTPUT_DIR}/Descriptors.txt ${OUTPUT_DIR}/descriptors
 
