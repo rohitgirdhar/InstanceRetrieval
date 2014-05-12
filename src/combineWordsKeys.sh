9be6f0c61015660b016a55971cce26cdbe67dd7f
Rohit Girdhar <rohit@iiith>
2014-05-12 18:42:29 +0530

    slash bug

--- src/combineWordsKeys.sh
+++ src/combineWordsKeys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dir=$1
-list=`cat ${dir}ImageNames.txt`
-mkdir -p ${dir}wordkeys
+list=`cat ${dir}/ImageNames.txt`
+mkdir -p ${dir}/wordkeys
 for i in $list
 do
-    paste ${dir}words/w_${i}.txt ${dir}keypoints/K_${i}.txt > ${dir}wordkeys/wk_$i.txt
+    paste ${dir}/words/w_${i}.txt ${dir}/keypoints/K_${i}.txt > ${dir}/wordkeys/wk_$i.txt
 done
